86fa220632d6ab5c80e39d56796958bfc7b6f75d
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-07-15 15:43:27 +0200

    feat: Add tiles method to take random tiles.

--- account_is_good.py
+++ account_is_good.py
@@ -7,5 +7,17 @@ def number_to_have():
     return secrets.randbelow(999 - 101 + 1) + 101
 
 
+def tiles():
+    present_tiles = list(range(1, 11)) * 2 + [25, 50, 75, 100]
+    take_tiles = []
+
+    for _ in range(6):
+        index = secrets.randbelow(len(present_tiles))
+        take_tiles.append(present_tiles[index])
+        present_tiles.pop(index)
+
+    return take_tiles
+
+
 if __name__ == "__main__":
     number_to_have()
