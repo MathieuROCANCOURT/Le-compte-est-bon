e6945200e41e6a4fce5237ca1dfbbf6861d9717e
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-07-15 15:15:50 +0200

    feat: Add generate number method between 101 and 999.

--- account_is_good.py
+++ account_is_good.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import secrets
+
+
+def number_to_have():
+    return secrets.randbelow(999 - 101 + 1) + 101
+
+
+if __name__ == "__main__":
+    number_to_have()
